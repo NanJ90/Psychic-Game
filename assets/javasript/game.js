8967e1f0f4976c3f019659190966b46fbe8c60a8
JJ <jianan.beijing@gmail.com>
2017-07-20 22:43:15 -0700

    fix disabled connecting between HTML and JS

--- assets/javasript/game.js
+++ assets/javasript/game.js
@@ -1,11 +1,18 @@
-var userGuess = document.getElementById("guess");
+	
+	var wins = 0;
+	var losses = 0;
+	var guessLeft = 9;
+
+window.addEventListener('load', function() { 
+	var userGuess = document.getElementById("guess");
 	
 	document.onkeyup = function(event) {
 	userGuess.textContent = event.key;
 
 	var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 	var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-	console.log(computerChoice);
+	
+	}
 
-		}
+		})
 	
