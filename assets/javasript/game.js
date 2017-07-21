974c5822474725217d4d64b3bb07121ba256425c
JJ <jianan.beijing@gmail.com>
2017-07-21 00:07:48 -0700

    fixing logic of calculation

--- assets/javasript/game.js
+++ assets/javasript/game.js
@@ -3,6 +3,8 @@
 	var losses = 0;
 	var guessLeft = 9;
 
+
+
 window.addEventListener('load', function() { 
 	var userGuess = document.getElementById("guess");
 	
@@ -11,8 +13,25 @@ window.addEventListener('load', function() {
 
 	var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 	var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-	
+	if (computerChoice !== userGuess){
+		if(guessLeft === 1){
+			losses++;
+			guessLeft= 9;
+			
+		}
+		else{
+			guessLeft--;
+			
+
+		}
+	}
+	else{
+		wins++;
+		guesLeft = 9; 
+
 	}
 
-		})
+	}
+
+	
 	
